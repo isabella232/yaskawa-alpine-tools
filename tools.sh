d4054585eb81cf2636c6b732e534d2978c7fe2b4
YASKAWA Europe GmbH <36499562+yeurplc@users.noreply.github.com>
2018-04-27 13:27:42 +0200

    apk no cache

--- tools.sh
+++ tools.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
-apk update && apk upgrade && apk add git nano mc
+# update && upgrade  system repos 
+# add git tools
+# add nano editor
+# add midnightcommander
+# clean apk cache ..just in case ..
+
+apk update && apk upgrade && apk --no-cache add git nano mc && apk cache clean
+
 
 echo "install done"
