5de37691592cc1aef0d4a76dcc68f7afd7cbe06e
YASKAWA Europe GmbH <36499562+yeurplc@users.noreply.github.com>
2018-04-30 08:47:44 +0200

    - apk cache clean

--- tools.sh
+++ tools.sh
@@ -4,9 +4,10 @@
 # add git tools
 # add nano editor
 # add midnightcommander
+
 # clean apk cache ..just in case ..
+# '&& apk cache clean'  run's into error with '--no-cache' option !
 
-apk update && apk upgrade && apk --no-cache add git nano mc && apk cache clean
-
+apk update && apk upgrade && apk --no-cache add git nano mc
 
 echo "install done"
